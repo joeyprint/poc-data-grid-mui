36413e023d63cdc57538add703ec4790c88d50cf
joeywasin <wasin.ww1402@gmail.com>
2022-02-02 18:00:58 +0700

    feat: try to test normal data grid component in app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Box, Typography } from "@mui/material";
 
-import DataGrid from "./Components/DataGrid";
+import { NormalDataGrid } from "./Components/DataGrid";
 import theme from "./Components/Theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <DataGrid />
+
+      {/* Normal Data Grid */}
+      <Typography variant="h6">Normal Data Grid</Typography>
+      <NormalDataGrid />
+
+      <Box mt={3} />
     </ThemeProvider>
   );
 }
